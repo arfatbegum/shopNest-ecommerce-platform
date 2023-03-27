ad86514f3ab0e26013bb5e861f96e1f041c13f39
arfat132 <arfatbegum90@gmail.com>
2023-03-27 23:24:17 +0300

    database connected

---
+++ backend/config/dbConeect.js
@@ -0,0 +1,11 @@
+const { mongoose } = require("mongoose");
+
+const dbConnect = () => {
+  try {
+    const conn = mongoose.connect(process.env.MONGODB_URL);
+    console.log("Database Connected Successfully");
+  } catch (error) {
+    console.log("Error from Database");
+  }
+};
+module.exports = dbConnect;
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,13 @@
 const express = require("express");
+const dbConnect = require("./config/dbConeect");
 const app = express();
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 5000;
+dbConnect();
+
+app.use("/", (req, res) => {
+    res.send("Server side is running")
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running  at PORT ${PORT}`);
