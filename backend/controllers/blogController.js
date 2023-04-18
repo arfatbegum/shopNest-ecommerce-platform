35e00739965b990bcfa83768d36a6ba8c5994054
arfat132 <arfatbegum90@gmail.com>
2023-04-19 02:09:50 +0300

    get a blog functionality created

--- backend/controllers/blogController.js
+++ backend/controllers/blogController.js
@@ -4,13 +4,32 @@ const asyncHandler = require("express-async-handler");
 const validateMongoDbId = require("../utils/validateMongoDbId");
 
 //Create a Blog
-const createBlog= asyncHandler(async (req, res) => {
+const createBlog = asyncHandler(async (req, res) => {
     try {
         if (req.body.title) {
             req.body.slug = slugify(req.body.name);
         }
         const newBlog = await Blog.create(req.body);
         res.json(newBlog);
+    } catch (error) {
+        throw new Error(error);
+    }
+});
+
+//Get a Blog
+const getBlog = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    validateMongoDbId(id);
+    try {
+        const updateViews = await Blog.findByIdAndUpdate(
+            id,
+            {
+                $inc: { views: 1 }
+            },
+            {
+                new: true
+            })
+        res.json(updateViews);
     } catch (error) {
         throw new Error(error);
     }
@@ -33,4 +52,4 @@ const updateBlog = asyncHandler(async (req, res) => {
     }
 });
 
-module.exports = { createBlog, updateBlog };
+module.exports = { createBlog, updateBlog, getBlog };
--- backend/routes/blogRoute.js
+++ backend/routes/blogRoute.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware");
-const { createBlog, updateBlog } = require("../controllers/blogController");
+const { createBlog, updateBlog, getBlog } = require("../controllers/blogController");
 const router = express.Router();
 
 
 router.post("/", authMiddleware, isAdmin, createBlog);
+router.get("/:id", getBlog);
 router.put("/:id", authMiddleware, isAdmin, updateBlog);
 
 module.exports = router;
