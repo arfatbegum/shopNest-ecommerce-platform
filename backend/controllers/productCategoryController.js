2ba57b5f98444e503dba6d523d91f308b8ef828a
arfat132 <arfatbegum90@gmail.com>
2023-04-20 01:28:32 +0300

    getAllProductCategory getProductCategory function created

--- backend/controllers/productCategoryController.js
+++ backend/controllers/productCategoryController.js
@@ -1,11 +1,24 @@
-const Category = require("../models/productCategoryModal");
+const ProductCategory = require("../models/productCategoryModal");
 const asyncHandler = require("express-async-handler");
+const validateMongoDbId = require("../utils/validateMongoDbId");
 
 //Create a product Category
 const createProductCategory = asyncHandler(async (req, res) => {
     try {
-        const newCategory = await Category.create(req.body);
+        const newCategory = await ProductCategory.create(req.body);
         res.json(newCategory);
+    } catch (error) {
+        throw new Error(error);
+    }
+});
+
+//Get a product Category
+const getProductCategory = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    validateMongoDbId(id);
+    try {
+        const getProductCategory = await ProductCategory.findById(id);
+        res.json(getProductCategory);
     } catch (error) {
         throw new Error(error);
     }
@@ -15,7 +28,7 @@ const createProductCategory = asyncHandler(async (req, res) => {
 const updateProductCategory = asyncHandler(async (req, res) => {
     const { _id } = req.params;
     try {
-        const updatedProductCategory = await Category.findOneAndUpdate(_id, req.body, {
+        const updatedProductCategory = await ProductCategory.findOneAndUpdate(_id, req.body, {
             new: true,
         });
         res.json(updatedProductCategory);
@@ -24,20 +37,34 @@ const updateProductCategory = asyncHandler(async (req, res) => {
     }
 });
 
-//Delete a product
+//Delete a product Category
 const deleteProductCategory = asyncHandler(async (req, res) => {
     const { _id } = req.params;
     console.log(_id);
     try {
-        const deletedProductCategory = await Category.findOneAndDelete(_id);
+        const deletedProductCategory = await ProductCategory.findOneAndDelete(_id);
         res.json(deletedProductCategory);
     } catch (error) {
         throw new Error(error);
     }
 });
 
+//Get All  product Category
+const getAllProductCategory = asyncHandler(async (req, res) => {
+    try {
+        const getAllProductCategory = await ProductCategory.find();
+        res.json(getAllProductCategory);
+    } catch (error) {
+        throw new Error(error);
+    }
+});
+
+
+
 module.exports = {
     createProductCategory,
     updateProductCategory,
-    deleteProductCategory
+    deleteProductCategory,
+    getProductCategory,
+    getAllProductCategory
 };
--- backend/routes/productCategoryRoute.js
+++ backend/routes/productCategoryRoute.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { createProductCategory, updateProductCategory, deleteProductCategory } = require("../controllers/productCategoryController");
+const { createProductCategory, updateProductCategory, deleteProductCategory, getProductCategory, getAllProductCategory } = require("../controllers/productCategoryController");
 const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware");
 const router = express.Router();
 
 router.post("/", authMiddleware, isAdmin, createProductCategory);
+router.get("/", getAllProductCategory);
+router.get("/:id", getProductCategory);
 router.put("/:id", authMiddleware, isAdmin, updateProductCategory);
 router.delete("/:id", authMiddleware, isAdmin, deleteProductCategory);
 
