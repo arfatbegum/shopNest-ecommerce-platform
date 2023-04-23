eb42eb0d7c289e9777b39285e8eca3f8b031db0f
arfat132 <arfatbegum90@gmail.com>
2023-04-23 20:04:18 +0300

    brand controllers added

---
+++ backend/controllers/brandController.js
@@ -0,0 +1,70 @@
+const Brand = require("../models/brandModal");
+const asyncHandler = require("express-async-handler");
+const validateMongoDbId = require("../utils/validateMongoDbId");
+
+//Create a Brand
+const createBrand = asyncHandler(async (req, res) => {
+    try {
+        const newBrand = await Brand.create(req.body);
+        res.json(newBrand);
+    } catch (error) {
+        throw new Error(error);
+    }
+});
+
+//Get a Brand
+const getBrand = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    validateMongoDbId(id);
+    try {
+        const getBrand = await Brand.findById(id);
+        res.json(getBrand);
+    } catch (error) {
+        throw new Error(error);
+    }
+});
+
+//Update a Brand
+const updateBrand = asyncHandler(async (req, res) => {
+    const { _id } = req.params;
+    try {
+        const updatedBrand = await Brand.findOneAndUpdate(_id, req.body, {
+            new: true,
+        });
+        res.json(updatedBrand);
+    } catch (error) {
+        throw new Error(error);
+    }
+});
+
+//Delete a Brand
+const deleteBrand = asyncHandler(async (req, res) => {
+    const { _id } = req.params;
+    console.log(_id);
+    try {
+        const deletedBrand = await Brand.findOneAndDelete(_id);
+        res.json(deletedBrand);
+    } catch (error) {
+        throw new Error(error);
+    }
+});
+
+//Get All Brand
+const getAllBrand = asyncHandler(async (req, res) => {
+    try {
+        const getAllBrand = await Brand.find();
+        res.json(getAllBrand);
+    } catch (error) {
+        throw new Error(error);
+    }
+});
+
+
+
+module.exports = {
+    createBrand,
+    updateBrand,
+    deleteBrand,
+    getBrand,
+    getAllBrand
+};
