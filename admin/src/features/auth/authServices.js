c1b8a64c67ebcf3efb0a70d93ca430172e37108e
arfat132 <arfatbegum90@gmail.com>
2023-06-04 20:24:48 +0300

    [DONE] Redux store and auth features

---
+++ admin/src/features/auth/authServices.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+import { base_url } from "../../utils/baseUrl";
+const login = async (user) => {
+  const response = await axios.post(`${base_url}user/admin-login`, user);
+  if (response.data) {
+    localStorage.setItem("user", JSON.stringify(response.data));
+  }
+  return response.data;
+};
+
+
+const authService = {
+  login,
+};
+
+export default authService;
