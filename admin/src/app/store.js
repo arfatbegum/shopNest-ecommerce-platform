350e057f802561fa4e7b668de279c023b4eca804
arfat132 <arfatbegum90@gmail.com>
2023-06-10 18:31:25 +0300

    [DONE] Brand slice and services

--- admin/src/app/store.js
+++ admin/src/app/store.js
@@ -3,11 +3,14 @@ import authReducer from "../features/auth/authSlice";
 import customerReducer from "../features/customers/customerSlice";
 import productReducer from "../features/product/productSlice";
 import productCategoryReducer from "../features/productCategories/productCategorySlice";
+import barndCategoryReducer from "../features/brand/brandSlice";
+
 export const store = configureStore({
     reducer: {
         auth: authReducer,
         customer: customerReducer,
         product: productReducer,
         productCategory: productCategoryReducer,
+        brandCategory: barndCategoryReducer,
     },
 });
---
+++ admin/src/features/brand/brandSlice.js
@@ -0,0 +1,150 @@
+import { createSlice, createAsyncThunk, createAction } from "@reduxjs/toolkit";
+import brandServices from "./brandServices";
+
+export const getBrands = createAsyncThunk(
+  "brand/get-brands",
+  async (thunkAPI) => {
+    try {
+      return await brandServices.getBrands();
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+export const getABrand = createAsyncThunk(
+  "brand/get-brand",
+  async (id, thunkAPI) => {
+    try {
+      return await brandServices.getBrand(id);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+export const createBrand = createAsyncThunk(
+  "brand/create-brand",
+  async (brandData, thunkAPI) => {
+    try {
+      return await brandServices.createBrand(brandData);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+export const updateABrand = createAsyncThunk(
+  "brand/update-brand",
+  async (brand, thunkAPI) => {
+    try {
+      return await brandServices.updateBrand(brand);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+
+export const deleteABrand = createAsyncThunk(
+  "brand/delete-brand",
+  async (id, thunkAPI) => {
+    try {
+      return await brandServices.deleteBrand(id);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+
+export const resetState = createAction("Reset_all");
+
+const initialState = {
+  brands: [],
+  isError: false,
+  isLoading: false,
+  isSuccess: false,
+  message: "",
+};
+export const brandSlice = createSlice({
+  name: "brands",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getBrands.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getBrands.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isError = false;
+        state.isSuccess = true;
+        state.brands = action.payload;
+      })
+      .addCase(getBrands.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.isSuccess = false;
+        state.message = action.error;
+      })
+      .addCase(createBrand.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(createBrand.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isError = false;
+        state.isSuccess = true;
+        state.createdBrand = action.payload;
+      })
+      .addCase(createBrand.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.isSuccess = false;
+        state.message = action.error;
+      })
+      .addCase(getABrand.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getABrand.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isError = false;
+        state.isSuccess = true;
+        state.brandName = action.payload.title;
+      })
+      .addCase(getABrand.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.isSuccess = false;
+        state.message = action.error;
+      })
+      .addCase(updateABrand.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(updateABrand.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isError = false;
+        state.isSuccess = true;
+        state.updatedBrand = action.payload;
+      })
+      .addCase(updateABrand.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.isSuccess = false;
+        state.message = action.error;
+      })
+      .addCase(deleteABrand.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(deleteABrand.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isError = false;
+        state.isSuccess = true;
+        state.deletedBrand = action.payload;
+      })
+      .addCase(deleteABrand.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.isSuccess = false;
+        state.message = action.error;
+      })
+      .addCase(resetState, () => initialState);
+  },
+});
+
+export default brandSlice.reducer;
